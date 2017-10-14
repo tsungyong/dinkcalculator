c7f185e549d8ef0f7c8c0c0914fe02d66a01cb6d
Claire R <clairepanak@gmail.com>
2017-10-14 20:22:13 +0000

    some errors

--- DINKCalculatorApp.py
+++ DINKCalculatorApp.py
@@ -27,8 +27,7 @@ def ene(name_var):
                 valid = True
             elif ene == 2:
                 income = salary(name_var)
-                valid = True    
-            break
+                valid = True
         except ValueError:
             print("Please try again.")
     return income
@@ -47,10 +46,10 @@ def wage(name_var):
     while wagevalid == False:
         wage_var = input("How much does " + name_var + " make per hour? ")
         try:
-            wage = float(wage)
+            wage_var = float(wage_var)
             wagevalid = True
         except ValueError:
-            wagevalid=False
+            wagevalid = False
     weekly = hours*wage_var
     monthly = weekly*4
     print(name_var + " makes " + '${:,.2f}'.format(weekly) + " per week and approximately " + '${:,.2f}'.format(monthly) + " per month.")
@@ -58,7 +57,14 @@ def wage(name_var):
 
 #pulls name_a or name_b from DINKcalculator()
 def salary(name_var):
-    yearly = float(input("What is " + name_var + "'s annual salary? "))
+    valid = False
+    while valid == False:
+        yearly = input("What is " + name_var + "'s annual salary? ")
+        try:
+            yearly = float(yearly)
+            valid = True
+        except ValueError:
+            valid = False
     monthly = yearly / 12
     print(name_var + " makes " + '${:,.2f}'.format(monthly) + " per month.")
     return monthly
@@ -81,47 +87,77 @@ def taxes(income):
         pct = .35
     else:
         pct = .396
-    taxes = tax_income*pct
+    taxes = tax_income*pct + tax_income*.0765
     return taxes
 
 def expenses():
     valid = False
+    r_valid = False
+    h_valid = False
+    t_valid = False
+    f_valid = False
+    d_valid = False
+    s_valid = False
     while valid == False:
-        exp_option = int(input("Enter 1 if you would like to itemize expenses or 2 to not: "))
-        #int input for each expense category
-        if exp_option == 1:
-            valid = True
-            rent = float(input("Monthly housing costs: "))
-            health = float(input("Monthly healthcare costs: "))
-            transportation = float(input("Monthly transportation costs: "))
-            food = float(input("Monthly food costs: "))
-            debt = float(input("Monthly debt payments: "))
-            shopping = float(input("Other monthly expenses: "))
-            total_expenses = rent + health + transportation + food + shopping + debt
-            #int input for total expenses
-        elif exp_option == 2:
-            valid = True
-            total_expenses = float(input("Total monthly expenses: "))
+        try:
+            exp_option = int(input("Enter 1 if you would like to itemize expenses or 2 to not: "))
+            #int input for each expense category
+            if exp_option == 1:
+                valid = True
+                while r_valid == False:
+                    try:
+                        rent = float(input("Monthly housing costs: "))
+                        r_valid = True
+                    except ValueError:
+                        print("Try again.")
+                while h_valid == False:
+                    try:
+                        health = float(input("Monthly healthcare costs: "))
+                        h_valid = True
+                    except ValueError:
+                        print("Try again.")
+                while t_valid == False:
+                    try:
+                        transportation = float(input("Monthly transportation costs: "))
+                        t_valid = True
+                    except ValueError:
+                        print("Try again.")
+                while f_valid == False:
+                    try:
+                        food = float(input("Monthly food costs: "))
+                        f_valid = True
+                    except ValueError:
+                        print("Try again.")
+                while d_valid == False:
+                    try:
+                        debt = float(input("Monthly debt payments: "))
+                        d_valid = True
+                    except ValueError:
+                        print("Try again.")
+                while s_valid == False:
+                    try:
+                        shopping = float(input("Other monthly expenses: "))
+                        s_valid = True
+                    except ValueError:
+                        print("Try again.")
+                total_expenses = rent + health + transportation + food + shopping + debt
+                #int input for total expenses
+            elif exp_option == 2:
+                valid = True
+                te_valid = False
+                while te_valid == False:
+                    try:
+                        total_expenses = float(input("Total monthly expenses: "))
+                        te_valid = True
+                    except ValueError:
+                        print("Try again.")
+        except ValueError:
+            print("Try again.")
     print("Total monthly expenses: " + '${:,.2f}'.format(total_expenses))
     return total_expenses
 
 def retirement(mon_save):
-    valid = False
-    balance = float(input("How much do you currently have saved for retirement? "))
-    years_till = int(input("How many years until retirement? "))
-    while valid == False:
-        rate_choice = int(input("Enter 1 to use default growth rate. Enter 2 to use a custom rate. "))
-        if rate_choice == 1:
-            valid = True
-            if years_till <= 10:
-                rate = .04
-            elif years_till <= 25:
-                rate = .05
-            else:
-                rate = .06
-        elif rate_choice == 2:
-            valid = True
-            rate = float(input("Enter an interest rate in decimals: "))
+    
     monthly_rate = rate/12
     n_months = years_till*12
     final_savings = balance*(1+monthly_rate)**n_months + ((1+monthly_rate)**n_months-1)/monthly_rate*mon_save
